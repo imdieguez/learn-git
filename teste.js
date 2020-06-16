db72cd52a0ee2b0a71e45b53c68aec5a86afaa1b
Marcelo Dieguez <marceloldieguez@gmail.com>
2020-06-16 15:04:12 -0300

    atualizando hoje o js.

--- teste.js
+++ teste.js
@@ -1,1 +1,6 @@
-let uihasd = 'iuhasd'
+let nome = 'Marcelo Dieguez';
+let idade = 33;
+
+if (idade < 99) {
+    return console.log(`Fala, ${nome}! Você tem ${idade} anos. Bem-vindo à programação!`)
+}
